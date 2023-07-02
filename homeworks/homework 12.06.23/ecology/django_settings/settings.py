2ae543f76ed6effde975ec259cd8659bbc7596a0
Abuka13 <abukaozbekbay@gmail.com>
2023-07-02 20:59:21 +0600

    Update
    Update

--- homeworks/homework 12.06.23/ecology/django_settings/settings.py
+++ homeworks/homework 12.06.23/ecology/django_settings/settings.py
@@ -111,4 +111,4 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'x   
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'x

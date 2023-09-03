f60531dc0b8f6c29262a12b576f5902ab135eb94
Abuka13 <abukaozbekbay@gmail.com>
2023-09-04 00:57:26 +0600

    update
    update

--- homeworks/homework 01.09.23/Подачу полезных предложений Django ORM/django_app/models.py
+++ homeworks/homework 01.09.23/Подачу полезных предложений Django ORM/django_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+class Suggests(models.Model):
+    author = models.CharField(max_length=300, verbose_name="Имя")
+    title = models.CharField(max_length=300, verbose_name="Наименование")
+    image = models.ImageField("Изображение", upload_to="images/posts")
+    description = models.TextField(verbose_name="Описание")
+    date = models.DateTimeField(auto_now_add=True, verbose_name="Дата")
 
-# Create your models here.
+    class Meta:
+        """Вспомогательный класс"""
+
+        app_label = "django_app"
+        ordering = ("-date", "title")
+        verbose_name = "Предложение"
+        verbose_name_plural = "Предложения"
+
+    def __str__(self):
+        return self.title   
--- homeworks/homework 01.09.23/Подачу полезных предложений Django ORM/django_app/views.py
+++ homeworks/homework 01.09.23/Подачу полезных предложений Django ORM/django_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django_app import models
 import sqlite3
 from django.http import HttpResponse, JsonResponse
 def home(request):
     return render(request, 'home.html')
 
+def list(request):
+    select_orm = models.Suggests.objects.all()
+    return render(request, 'list.html', context={"select_orm": select_orm})
